8d264c3eadb65f69ab0cf094a4e494261ddcd8a1
hao <alien@gitUse.com>
2017-12-20 11:36:19 +0800

    delete some test files and modifiedindex.js file

--- react-demo/react-demo/src/index.js
+++ react-demo/react-demo/src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './css/test.css';
 import Test from './js/component/test';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Test />, document.getElementById('root'));
-// registerServiceWorker();
